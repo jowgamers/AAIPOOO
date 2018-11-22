49d2412025f05cf834c0eca32b7b7ed8ae9e7be2
jowgamers <jowgamers@hotmail.com>
2018-11-22 17:22:56 -0200

    Add files via upload

--- JoaoAAI/src/controller/ItemCompraDAO.java
+++ JoaoAAI/src/controller/ItemCompraDAO.java
@@ -1,5 +1,113 @@
 package controller;
 
-public class ItemCompraDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JOptionPane;
+
+import connection.ConnectionManager;
+import model.ItemCompra;
+import model.Produto;
+
+public class ItemCompraDAO {
+	public void create(ItemCompra i) {
+
+		Connection con = null;
+
+		try {
+			con = ConnectionManager.getMysqlConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		PreparedStatement stmt = null;
+
+		// Falta fechar a conexão com o banco
+		try {
+			stmt = con.prepareStatement(
+					"INSERT INTO itemcompra (cod_prod, qnt_cmp, vlr_cmp, cmp_fnl) VALUES (?, ?, ?, N");
+			
+			stmt.setInt(1, i.getProduto());
+			stmt.setDouble(2, i.getQuantCompra());
+			stmt.setDouble(3, i.getValorCompra());
+			stmt.executeUpdate();
+
+			JOptionPane.showMessageDialog(null, "Item adicionado ao carrinho", "Acões BD",
+					JOptionPane.PLAIN_MESSAGE);
+
+		} catch (SQLException e) {
+
+			JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto no carrinho " + e, "Acões BD", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+
+	}
+
+	public List<ItemCompra> read() {
+
+		Connection con = null;
+
+		try {
+			con = ConnectionManager.getMysqlConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+
+		List<ItemCompra> ItemCompra = new ArrayList<ItemCompra>();
+
+		try {
+			stmt = con.prepareStatement("SELECT * FROM ItemCompra");
+			rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				ItemCompra iC = new ItemCompra();
+				iC.setCodCompra(rs.getInt("num_cmp"));
+				iC.setProduto(rs.getInt("cod_prod"));
+				iC.setQuantCompra(rs.getInt("cod_prod"));
+				iC.setValorCompra(rs.getDouble("cod_prod"));
+				iC.setCompraFinalizada(rs.getString("cmp_fnl"));
+
+				ItemCompra.add(iC);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ItemCompra;
+
+	}
+
+	public void delete(ItemCompra iC) {
+
+		Connection con = null;
+
+		try {
+			con = ConnectionManager.getMysqlConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		PreparedStatement stmt = null;
+
+		try {
+			stmt = con.prepareStatement("DELETE FROM ItemCompra WHERE num_cmp = ?");
+			stmt.setInt(1, iC.getProduto());
+			stmt.executeUpdate();
+
+			JOptionPane.showMessageDialog(null, "Produto excluido com sucesso", "Acões BD", JOptionPane.PLAIN_MESSAGE);
+
+		} catch (SQLException e) {
+
+			JOptionPane.showMessageDialog(null, "Erro ao excluir produto " + e, "Acões BD", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+
+	}
 }
--- JoaoAAI/src/model/ItemCompra.java
+++ JoaoAAI/src/model/ItemCompra.java
@@ -2,42 +2,59 @@ package model;
 
 public class ItemCompra {
 	
-	private Produto produto;
+	private int codCompra;
+	private int produto;
 	private int quantCompra;
 	private double valorCompra;
-	
-	public ItemCompra(Produto produto, int quantCompra, double valorCompra) {
-		super();
-		this.produto = produto;
-		this.quantCompra = quantCompra;
-		this.valorCompra = valorCompra;
-	}
-	
+	private String compraFinalizada;
 	
 	public ItemCompra () {
 		
 	}
+	
+	
+	public ItemCompra(int produto, int quantCompra, double valorCompra, String compraFinalizada, int codCompra) {
+		super();
+		this.produto = produto;
+		this.quantCompra = quantCompra;
+		this.valorCompra = valorCompra;
+		this.compraFinalizada = compraFinalizada;
+		this.codCompra = codCompra; 
+	}
 
-	//
-	public Produto getProduto() {
+	public int getCodCompra() {
+		return codCompra;
+	}
+
+
+	public void setCodCompra(int codCompra) {
+		this.codCompra = codCompra;
+	}
+	
+	public int getProduto() {
 		return produto;
 	}
 
-	public void setProduto(Produto produto) {
+
+	public void setProduto(int produto) {
 		this.produto = produto;
 	}
+
 
 	public int getQuantCompra() {
 		return quantCompra;
 	}
 
+
 	public void setQuantCompra(int quantCompra) {
 		this.quantCompra = quantCompra;
 	}
 
+
 	public double getValorCompra() {
 		return valorCompra;
 	}
+
 
 	public void setValorCompra(double valorCompra) {
 		this.valorCompra = valorCompra;
@@ -47,6 +64,12 @@ public class ItemCompra {
 	@Override
 	public String toString() {
 		return "ItemCompra [produto=" + produto + ", quantCompra=" + quantCompra + ", valorCompra=" + valorCompra + "]";
+	}
+	public String getCompraFinalizada() {
+		return compraFinalizada;
+	}
+	public void setCompraFinalizada(String compraFinalizada) {
+		this.compraFinalizada = compraFinalizada;
 	}
 	
 	
